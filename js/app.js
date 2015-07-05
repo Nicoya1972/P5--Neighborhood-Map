4da7e42e6db88fedaedfa91341c3d797dc0b5266
Nicoya1972 <Nicoya1972@yahoo.com>
2015-07-05 15:32:57 -0700

    initial commit
    P5 initial commit

--- js/app.js
+++ js/app.js
@@ -1,76 +1,14 @@
-$(function() {
+function initialize() {
 
-    var data = {
-        lastID: 0,
-        pizzas: []
-    };
+    var mapOptions = {
+        center : { lat: 37.940837, lng: -121.934608},
+        zoom: 16
+      };
 
-
-    var octopus = {
-        addPizza: function() {
-            var thisID = ++data.lastID;
-
-            data.pizzas.push({
-                id: thisID,
-                visible: true
-            });
-            view.render();
-        },
-
-        removePizza: function(pizza) {
-            var clickedPizza = data.pizzas[ pizza.id - 1 ];
-            clickedPizza.visible = false;
-            view.render();
-        },
-
-        getVisiblePizzas: function() {
-            var visiblePizzas = data.pizzas.filter(function(pizza) {
-                return pizza.visible;
-            });
-            return visiblePizzas;
-        },
-
-        init: function() {
-            view.init();
-        }
+      map = new google.maps.Map(document.getElementById('map-canvas'), 
+        mapOptions);
     }
-
-
-    var view = {
-        init: function() {
-            var addPizzaBtn = $('.add-pizza');
-            addPizzaBtn.click(function() {
-                octopus.addPizza();
-            });
-
-            // grab elements and html for using in the render function
-            this.$pizzaList = $('.pizza-list');
-            this.pizzaTemplate = $('script[data-template="pizza"]').html();
-
-            // Delegated event to listen for removal clicks
-            this.$pizzaList.on('click', '.remove-pizza', function(e) {
-                var pizza = $(this).parents('.pizza').data();
-                octopus.removePizza(pizza);
-                return false;
-            });
-
-            this.render();
-        },
-
-        render: function() {
-            // Cache vars for use in forEach() callback (performance)
-            var $pizzaList = this.$pizzaList,
-                pizzaTemplate = this.pizzaTemplate;
-
-            // Clear and render
-            $pizzaList.html('');
-            octopus.getVisiblePizzas().forEach(function(pizza) {
-                // Replace template markers with data
-                var thisTemplate = pizzaTemplate.replace(/{{id}}/g, pizza.id);
-                $pizzaList.append(thisTemplate);
-            });
-        }
-    };
-
-    octopus.init();
-}());
+    
+    google.maps.event.addDomListener(window, 'load', initialize);
+   
+    
